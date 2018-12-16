7921421a4f1c5e4e4e617628268b8e551c7c8443
jurgis.ruza@epfl.ch <jurgis.ruza@epfl.ch>
2018-12-16 22:56:36 +0100

    sentiment

--- lib/sentiment.py
+++ lib/sentiment.py
@@ -6,6 +6,8 @@ Thsi file contains content related to sentiment anylsis.
 
 import pandas as pd
 import numpy  as np
+import numpy as np
+import matplotlib.pyplot as plt
 from tqdm import tqdm_notebook
 
 
@@ -46,3 +48,54 @@ def add_sentiment_data(df_reports, sentiment_file='sentiment.csv'):
     df_merged_report.rename(columns={'Filename': 'url'}, inplace=True)
     df_merged_report = df_merged_report.set_index('url')
     return df_merged_report
+
+def get_emotion_data(df):
+    df_longer_reports = df[df['WC'] > 100].copy()
+    df_longer_reports.Shape = df_longer_reports.Shape.str.replace('([ ]+)', '')
+    df_longer_reports.Shape = df_longer_reports.Shape.str.title()
+    return df_longer_reports
+    
+def emotions_boxplot(df, showfliers=True):    
+    emotions = ['family','friend','posemo','negemo','anger','sad',
+                'anx','hear','feel','reward','achieve','risk',
+                'tentat','certain']
+    df_emotions = df[emotions]
+    emotions_values = []
+    for emo in emotions:
+        emotions_values.append(df_emotions[emo].values)
+    fig, ax = plt.subplots(figsize=(15, 6))
+    
+    ax = plt.boxplot(x = emotions_values, showfliers=showfliers)
+    plt.ylabel('Percentage', fontsize=14)
+    plt.title('Emotions',fontsize=18)
+    plt.xticks(list(range(1,len(emotions)+1)), emotions)
+    plt.show()
+    
+def plot_sentiment_nets(df):
+    emotions_labels = ['feel','negemo','hear','posemo','reward','certain']
+    shape_labels = ['Light', 'Triangle', 'Circle', 'Other', 'Unknown', 'Sphere']
+    shape_stats = []
+    shapes_list = []
+    for shape in shape_labels:
+        df_shape = df[(df.Shape == shape)].copy()
+        shapes_list.append(df_shape[emotions_labels])
+        for emo in emotions_labels:
+            shape_stats.append(shapes_list[-1][emo].mean())
+    shape_stats = np.asarray(shape_stats).reshape(6,6)
+
+    angles=np.linspace(0, 2*np.pi, len(emotions_labels), endpoint=False)
+    angles = np.concatenate((angles,[angles[0]]))
+    stats = []
+    for indice in range(6):
+        stats.append(np.concatenate((shape_stats[indice], 
+                                              [shape_stats[indice][0]])))  
+
+    fig=plt.figure(figsize=(15,15))
+    for i in range(6):
+        ax = fig.add_subplot(321+i, polar=True)
+        ax.plot(angles, stats[i], 'o-', linewidth=2, color='#751947')
+        ax.fill(angles, stats[i], alpha=0.25,color='#751947')
+        ax.set_thetagrids(angles * 180/np.pi, emotions_labels, fontsize=13)
+        ax.set_title(shape_labels[i], fontsize=16)
+        ax.grid(True)
+    plt.show()
